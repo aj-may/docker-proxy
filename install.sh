8d25b5290c358dfe7542ed6d3b86c2ccef13cf08
Mark Sost <sostmark@gmail.com>
2017-05-05 10:16:17 -0400

    Revert && removal

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-curl -Lo docker-proxy https://raw.githubusercontent.com/aj-may/docker-proxy/master/docker-proxy
-chmod +x docker-proxy
+curl -Lo docker-proxy https://raw.githubusercontent.com/aj-may/docker-proxy/master/docker-proxy && chmod +x docker-proxy
